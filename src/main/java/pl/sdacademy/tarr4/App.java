26e42a1a2ea93f2fda6456fc6a6c5f5dd0e0ccdb
Jakub Olszewski <jakub@olszewski.pl>
2019-02-05 19:56:57 +0100

    Tworzenie zmiennych i metod

--- src/main/java/pl/sdacademy/tarr4/App.java
+++ src/main/java/pl/sdacademy/tarr4/App.java
@@ -1,3 +1,4 @@
+package pl.sdacademy.tarr4;
 //generate test
 //alt + enter
 public class App {
@@ -12,5 +13,21 @@ public class App {
     public static void main(String[] args) {
         //sout + tab
         System.out.println("Witaj Świecie !");
+
+        int number = 2019;
+
+        int number2;
+        number2=2020;
+
+        System.out.println(number);
+        System.out.println(number2);
+
+        int pojemnik;
+        pojemnik = sumaLiczb(1,2,3);
+        System.out.println(pojemnik);
+    }
+
+    static int sumaLiczb(int a, int b, int c){
+        return a+b+c;
     }
 }
