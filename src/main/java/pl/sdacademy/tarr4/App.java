a65d81d45d18891f5396eb791d60aaf2ab90e494
Jakub Olszewski <jakub@olszewski.pl>
2019-02-05 16:48:49 +0100

    Init

---
+++ src/main/java/pl/sdacademy/tarr4/App.java
@@ -0,0 +1,16 @@
+//generate test
+//alt + enter
+public class App {
+
+    //code format
+    //ctrl+alt+L
+
+    // alt + insert + enter
+    // generate code
+
+    //psvm + tab
+    public static void main(String[] args) {
+        //sout + tab
+        System.out.println("Witaj Świecie !");
+    }
+}
