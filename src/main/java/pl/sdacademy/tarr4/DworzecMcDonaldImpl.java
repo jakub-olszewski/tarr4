7a04d35692d5675542cc51198bd78d085ca8d0f6
Jakub Olszewski <jakub@olszewski.pl>
2019-02-05 20:58:16 +0100

    Zadanie: Stworz metode poproszeFrytki

---
+++ src/main/java/pl/sdacademy/tarr4/DworzecMcDonaldImpl.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.tarr4;
+
+/**
+ * DworzecMcDonaldImpl
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 05.02.2019 20:56
+ **/
+public class DworzecMcDonaldImpl implements McDonald{
+
+    // Zadanie: Dodaj implementację metody z interfejsu
+    // metoda wypisuje tekst "Frytki"
+}
---
+++ src/main/java/pl/sdacademy/tarr4/McDonald.java
@@ -0,0 +1,12 @@
+package pl.sdacademy.tarr4;
+
+/**
+ * McDonald
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 05.02.2019 20:55
+ **/
+public interface McDonald {
+
+    //Zadanie: Stworz metode poproszeFrytki
+}
