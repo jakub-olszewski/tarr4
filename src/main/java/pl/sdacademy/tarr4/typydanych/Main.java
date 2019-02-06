a594b851210ec8aba7284a221eaf898dfd7d0787
Jakub Olszewski <jakub@olszewski.pl>
2019-02-06 18:37:29 +0100

    Rozwiazanie: Utwórz zmienna a typu Integer oraz zmienna b typu int

--- src/main/java/pl/sdacademy/tarr4/typydanych/Main.java
+++ src/main/java/pl/sdacademy/tarr4/typydanych/Main.java
@@ -10,13 +10,26 @@ public class Main {
 
     public static void main(String[] args) {
         // Zadanie: Utwórz zmienna a typu Integer oraz zmienna b typu int
+        Integer a;
+        int b;
+        a = 2;
+        b = 2;
         // dodaj zmienne i wynik przypisz do zmiennej c typu int
+        int c = a+b;
         // wywolaj metode equals() na zmiennej a
         // jako podaj argument metody podaj b
         // przypisz wynik metody equals() do zmiennej typu boolean
         // o nazwie wynikLogiczny
+        boolean wynikLogiczny;
+        wynikLogiczny = a.equals(b);
+        //boolean wynikLogiczny2 = b.equals(a); // <-- to nie zadziala bo b jest prymitywem
         // wypisz zmienna wynik logiczny na konsole.
+        System.out.println(wynikLogiczny);
 
+        // Uwaga!
         // metoda equals() porównuje a.equals(b) porównuje a z b
+
+
+
     }
 }
