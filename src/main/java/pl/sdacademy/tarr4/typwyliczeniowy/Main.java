e8f609137d8f3e92cdda3e81202bc5e825022d09
Jakub Olszewski <jakub@olszewski.pl>
2019-02-07 19:59:07 +0100

    Zadanie z rozwiazaniem: Typ wyliczeniowy

--- src/main/java/pl/sdacademy/tarr4/typwyliczeniowy/Main.java
+++ src/main/java/pl/sdacademy/tarr4/typwyliczeniowy/Main.java
@@ -10,9 +10,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // jedna wartość typu wyliczeniowego
         Status zmiennaTypuWyliczeniowego = Status.AKTYWNY;
+
+        // zamiast wielu wartości np napisu
         String status1 = "Aktywny";
         String status2 = "aktywny";
         String status3 = "atywnyy";
+
+        Plec plecUzytkownika = Plec.MEZCZYZNA;
+        System.out.println("Płeć użytkownika    " +plecUzytkownika);
+
     }
 }
---
+++ src/main/java/pl/sdacademy/tarr4/typwyliczeniowy/Plec.java
@@ -0,0 +1,11 @@
+package pl.sdacademy.tarr4.typwyliczeniowy;
+
+/**
+ * Plec
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 07.02.2019 19:52
+ **/
+public enum Plec {
+    KOBIETA, MEZCZYZNA;
+}
