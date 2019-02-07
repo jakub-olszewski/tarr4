24c90f398b28db1d0682439efd07fea233855de1
Jakub Olszewski <jakub@olszewski.pl>
2019-02-07 17:48:15 +0100

    Zadanie: Operatory

---
+++ src/main/java/pl/sdacademy/tarr4/operatory/Zadanie.java
@@ -0,0 +1,19 @@
+package pl.sdacademy.tarr4.operatory;
+
+/**
+ * Zadanie
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 07.02.2019 17:46
+ **/
+public class Zadanie {
+    public static void main(String[] args) {
+        /**
+         * Zaimplementuj  metody
+         * wykorzystując wcześniej poznane operatory.
+         * Metody : liczba parzysta, liczba ujemna
+         * Metody zwracają prawdę lub fałsz
+         * Metody przyjmują argument liczba całkowita
+         */
+    }
+}
