d226e2dc52e608049c61e10b88fa7e7b97954360
Jakub Olszewski <jakub@olszewski.pl>
2019-02-07 19:04:32 +0100

    Rozwiązanie: Utworz metodę sprawdzającą pełnoletniość z użyciem instrukcji warunkowej

--- src/main/java/pl/sdacademy/tarr4/instrukcje/Zadanie.java
+++ src/main/java/pl/sdacademy/tarr4/instrukcje/Zadanie.java
@@ -11,5 +11,46 @@ public class Zadanie {
         /**
          * Utwórz metodę sprawdzającą pełnoletniość
          */
+
+        boolean jestPelnolenia = czyPelnoletnia(20);
+        System.out.println("Powinno być true :"+jestPelnolenia);
+
+        /**
+         * Utwórz metodę sprawdzającą pełnoletniość i czy jest z Bydgoszczy
+         */
+        boolean jestPelnoletniaZBydgoszczy = czyPelnoletniaZBydgoszczy(20,"Toruń");
+        System.out.println("Powinno być false :"+jestPelnoletniaZBydgoszczy);
+    }
+
+    private static boolean czyPelnoletniaZBydgoszczy(int wiek, String miasto) {
+        if(wiek >= 18 && miasto =="Bydgoszcz") {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    private static boolean czyPelnoletniaZBydgoszczy2(int wiek, String miasto) {
+        return wiek >= 18 && miasto == "Bydgoszcz";
+    }
+
+    private static boolean czyPelnoletniaZBydgoszczy3(int wiek, String miasto) {
+        return wiek >= 18 && miasto.equalsIgnoreCase("bydgoszcz");
+    }
+
+    private static boolean czyPelnoletniaZBydgoszczy4(int wiek, String miasto) {
+        return czyPelnoletnia(wiek) && jestZBydgoszczy(miasto);
+    }
+
+    private static boolean jestZBydgoszczy(String miasto) {
+        return miasto.equalsIgnoreCase("bydgoszcz");
+    }
+
+    private static boolean czyPelnoletnia(int wiek) {
+        if(wiek >= 18){
+            return true;
+        }else {
+            return false;
+        }
     }
 }
