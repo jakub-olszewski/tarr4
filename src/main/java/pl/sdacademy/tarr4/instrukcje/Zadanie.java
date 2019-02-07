91a85ef2fe66e63cc5db1896f53330b6ce8ccb4a
Jakub Olszewski <jakub@olszewski.pl>
2019-02-07 18:40:45 +0100

    Zadanie: Utworz metodę sprawdzającą pełnoletniość z użyciem instrukcji warunkowej

---
+++ src/main/java/pl/sdacademy/tarr4/instrukcje/Zadanie.java
@@ -0,0 +1,15 @@
+package pl.sdacademy.tarr4.instrukcje;
+
+/**
+ * Zadanie
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 07.02.2019 18:39
+ **/
+public class Zadanie {
+    public static void main(String[] args) {
+        /**
+         * Utwórz metodę sprawdzającą pełnoletniość
+         */
+    }
+}
