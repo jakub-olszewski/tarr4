40ae8fabaa7e37172f992a5f87925ea93d6647db
Jakub Olszewski <jakub@olszewski.pl>
2019-02-07 19:33:08 +0100

    Teoria: Typ wyliczeniowy

---
+++ src/main/java/pl/sdacademy/tarr4/typwyliczeniowy/Main.java
@@ -0,0 +1,18 @@
+package pl.sdacademy.tarr4.typwyliczeniowy;
+
+/**
+ * Main
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 07.02.2019 19:26
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+
+        Status zmiennaTypuWyliczeniowego = Status.AKTYWNY;
+        String status1 = "Aktywny";
+        String status2 = "aktywny";
+        String status3 = "atywnyy";
+    }
+}
