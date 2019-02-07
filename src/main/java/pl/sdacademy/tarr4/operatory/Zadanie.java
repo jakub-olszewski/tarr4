d5fa20fea042813f282376c01aebec6a50b47fd9
Jakub Olszewski <jakub@olszewski.pl>
2019-02-07 18:18:24 +0100

    Rozwiązanie: Operatory

--- src/main/java/pl/sdacademy/tarr4/operatory/Zadanie.java
+++ src/main/java/pl/sdacademy/tarr4/operatory/Zadanie.java
@@ -15,5 +15,31 @@ public class Zadanie {
          * Metody zwracają prawdę lub fałsz
          * Metody przyjmują argument liczba całkowita
          */
+        boolean liczbaJestParzysta = liczbaParzysta(4);
+        System.out.println("Powinno być true :"+liczbaJestParzysta);
+
+        boolean liczbaNieJestParzysta = liczbaParzysta(5);
+        System.out.println("Powinno być true :"+!liczbaNieJestParzysta);
+        boolean toJestPrawda = !false;
+
+        boolean pytanie = !((5<3 || 4>100) && 4<123);
+        System.out.println("Odpowiedz na pytanie: "+pytanie);
+
+        boolean liczbaJestUjemna = liczbaUjemna(-3);
+        System.out.println("Powinno być true :"+liczbaJestUjemna);
+
+        boolean liczbaNieJestUjemna = liczbaUjemna(3);
+        System.out.println("Powinno być true :"+!liczbaNieJestUjemna);
+
     }
+
+    private static boolean liczbaUjemna(int i) {
+        return i<0;
+    }
+
+    private static boolean liczbaParzysta(int i) {
+        return i%2==0;
+    }
+
+
 }
