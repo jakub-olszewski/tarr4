d20ef796084cbe3f21f8e5c4a9a2dfd634f940fc
Jakub Olszewski <jakub@olszewski.pl>
2019-02-09 13:58:37 +0100

    Zadanie: Skaner i pobieranie z klawiatury

---
+++ src/main/java/pl/sdacademy/tarr4/scanner/Zadanie.java
@@ -0,0 +1,24 @@
+package pl.sdacademy.tarr4.scanner;
+
+/**
+ * Zadanie
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 09.02.2019 13:47
+ **/
+public class Zadanie {
+    public static void main(String[] args) {
+
+        /**
+         * pobrać 5 liczb w postaci listy
+         * z użyciem pętli
+         * i wypisać ujemne z użyciem pętli
+         * gdy nie ma ujemnych wypisuje
+         * stosowną informację o braku.
+         *
+         * nextInt() - pobieranie liczby
+         * nextLine() - pobranie tekstu
+         */
+
+    }
+}
