bf8692daefacf400566a0197e3416de4e18d0907
Jakub Olszewski <jakub@olszewski.pl>
2019-02-09 10:19:02 +0100

    Rozwiązanie: Refaktoring tablicy PozycjeZamowienia

--- src/main/java/pl/sdacademy/tarr4/kolekcje/Zadanie.java
+++ src/main/java/pl/sdacademy/tarr4/kolekcje/Zadanie.java
@@ -1,5 +1,11 @@
 package pl.sdacademy.tarr4.kolekcje;
 
+import javafx.stage.Popup;
+import pl.sdacademy.tarr4.obiekty.PozycjaZamowienia;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Zadanie
  *
@@ -11,7 +17,20 @@ public class Zadanie {
         /**
          * Zadanie: Refaktoring tablicy PozycjeZamowienia
          * z wykorzystaniem odpowiedniej kolekcji
-         * oraz wyświetlenie wybranego elementu z kolekcji.
+         * oraz wyświetlenie wybrane()o elementu z kolekcji.
          */
+        List<PozycjaZamowienia> listaZamowien = new ArrayList<PozycjaZamowienia>();
+        //List listPrzebojow;
+        //List<Object> listPrzebojow2;
+        listaZamowien.add(new PozycjaZamowienia("Papier A4",11.15f));
+        listaZamowien.add(new PozycjaZamowienia("kanapka",16.99f));
+        listaZamowien.add(new PozycjaZamowienia("frytki",6.99f));
+        listaZamowien.add(new PozycjaZamowienia("pepsi",4.99f));
+        listaZamowien.add(new PozycjaZamowienia("fanta",6.99f));
+        listaZamowien.add(new PozycjaZamowienia("woda",8.50f));
+
+        PozycjaZamowienia pozycja4 = listaZamowien .get(3);
+        System.out.println(pozycja4);
+
     }
 }
