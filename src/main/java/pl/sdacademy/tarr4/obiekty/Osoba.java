b2380a55ea57056d5da2c390d074a97e5ad7f900
Jakub Olszewski <jakub@olszewski.pl>
2019-02-09 09:35:33 +0100

    Zadanie: Refaktoring tablicy PozycjeZamowienia

--- src/main/java/pl/sdacademy/tarr4/obiekty/Osoba.java
+++ src/main/java/pl/sdacademy/tarr4/obiekty/Osoba.java
@@ -7,4 +7,19 @@ package pl.sdacademy.tarr4.obiekty;
  * @date: 08.02.2019 20:16
  **/
 public class Osoba {
+    private final String imie;
+    private final String nazwisko;
+
+    public Osoba(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
 }
