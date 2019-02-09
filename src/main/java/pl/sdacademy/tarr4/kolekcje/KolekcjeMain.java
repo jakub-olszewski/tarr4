88afe06810cad1bda5f3f04ced7a5f5b733de3a0
Jakub Olszewski <jakub@olszewski.pl>
2019-02-09 09:21:57 +0100

    Zadanie: Refaktoring tablicy PozycjeZamowienia

--- src/main/java/pl/sdacademy/tarr4/kolekcje/KolekcjeMain.java
+++ src/main/java/pl/sdacademy/tarr4/kolekcje/KolekcjeMain.java
@@ -14,21 +14,37 @@ public class KolekcjeMain {
     public static void main(String[] args) {
 
         // listy
-        List listaOsob = new ArrayList<Osoba>();// lista osób
+        // ==================================
+        List<Osoba> listaOsob = new ArrayList<Osoba>();// lista osób
+        List listaOsobInnaImplementacja = new LinkedList();// lista osób innej implementacji wykonania metod
+
         // lista elementów typu Osoba
-        listaOsob.add(new Osoba());
+        listaOsob.add(new Osoba("Cezary","Cezary"));
+        Osoba pobranaOsoba = listaOsob.get(0);
 
         // sety - zbiory
-        Set zbiorOsob = new HashSet<Osoba>();
-        zbiorOsob.add(new Osoba());
+        // ==================================
+
+        Set<Osoba> zbiorOsob = new HashSet<Osoba>();
+        zbiorOsob.add(new Osoba("Anna","Nowak"));
+        Osoba pobranaOsobaZeZbioru = zbiorOsob.iterator().next();
 
         // Mapy
+        // ==================================
         Map<String,String> slownik = new HashMap<String,String>();
         slownik.put("Krzesło","Chair");
+        String tlumaczenie = slownik.get("Krzesło");
+
+        Map<Long,Osoba> mapaOsobZPeselem = new HashMap<Long,Osoba>();
+        mapaOsobZPeselem.put(394889098223L,new Osoba("Jan","Kowalski"));
+        // pobranie osoby po numerze pesel
+        Osoba osobaPoNumerzePesel = mapaOsobZPeselem.get(29382039482L);
 
         // kolejki
-        Queue<Osoba> kolejkaOsob = null;
-        kolejkaOsob.add(new Osoba());
-        
+        // ==================================
+        Queue<Osoba> kolejkaOsob = new LinkedList();
+        kolejkaOsob.add(new Osoba("Mikołaj","Kopernik"));
+        Osoba osobaZKolejki = kolejkaOsob.peek();
+
     }
 }
---
+++ src/main/java/pl/sdacademy/tarr4/kolekcje/Zadanie.java
@@ -0,0 +1,17 @@
+package pl.sdacademy.tarr4.kolekcje;
+
+/**
+ * Zadanie
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 09.02.2019 09:16
+ **/
+public class Zadanie {
+    public static void main(String[] args) {
+        /**
+         * Zadanie: Refaktoring tablicy PozycjeZamowienia
+         * z wykorzystaniem odpowiedniej kolekcji
+         * oraz wyświetlenie wybranego elementu z kolekcji.
+         */
+    }
+}
