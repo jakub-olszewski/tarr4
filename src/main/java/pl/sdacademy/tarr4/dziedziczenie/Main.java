55996dae92686e10be61d71f7e34da5b6f39127d
Jakub Olszewski <jakub@olszewski.pl>
2019-02-09 15:57:01 +0100

    Zadania: Dziedziczenie i abstrakcja

--- src/main/java/pl/sdacademy/tarr4/dziedziczenie/Main.java
+++ src/main/java/pl/sdacademy/tarr4/dziedziczenie/Main.java
@@ -1,5 +1,10 @@
 package pl.sdacademy.tarr4.dziedziczenie;
 
+import pl.sdacademy.tarr4.dziedziczenie.figury.Kolo;
+import pl.sdacademy.tarr4.dziedziczenie.figury.Kwadrat;
+import pl.sdacademy.tarr4.dziedziczenie.figury.Prostokat;
+import pl.sdacademy.tarr4.dziedziczenie.figury.Trojkat;
+
 /**
  * Main
  *
@@ -16,5 +21,23 @@ public class Main {
          * posiada konstruktor z danymi do obliczenia obwodu
          * pola - niezbędne do obliczenia obwodu
          */
+        Kwadrat kwadrat = new Kwadrat(4f);
+        Trojkat trojkat = new Trojkat(3f,4f,5f);
+        Prostokat prostokat = new Prostokat(4f,3f);
+        Kolo kolo = new Kolo(5f);
+
+        // Zadanie 1
+        // stwórz klase abstrakcyjną Figura
+        // public abstract class Figura{}
+
+        // Zadanie 2
+        // stwórz listę elementów Figura
+        // dodaj powyżej figury do listy
+
+        // Zadanie 3
+        // w pętli zsumuj obwody wszystkich figur
+        // wypisz sumę
+
+
     }
 }
