756106ca81f004ee78fc6b5135f4933f59224d1a
Jakub Olszewski <jakub@olszewski.pl>
2019-02-09 15:03:59 +0100

    Zadanie: Figury i dziedziczenie

---
+++ src/main/java/pl/sdacademy/tarr4/dziedziczenie/Main.java
@@ -0,0 +1,20 @@
+package pl.sdacademy.tarr4.dziedziczenie;
+
+/**
+ * Main
+ *
+ * @author: Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @date: 09.02.2019 15:01
+ **/
+public class Main {
+    public static void main(String[] args) {
+
+        /**
+         * Zadanie: Stwórz klasy Kwadrat, Koło, Prostokąt i Trójkąt
+         * Każda klasa posiada metodę:
+         * obliczObwod(); - oblicza i zwraca obwód
+         * posiada konstruktor z danymi do obliczenia obwodu
+         * pola - niezbędne do obliczenia obwodu
+         */
+    }
+}
