8d661c9cce6f95baaec1970c9a86752d5fab4f71
Jakub Olszewski <jakub@olszewski.pl>
2019-02-10 15:33:19 +0100

    Projekt: Lista studentów - dodawanie

--- src/main/java/pl/sdacademy/tarr4/projekty/listStudents/views/AddView.java
+++ src/main/java/pl/sdacademy/tarr4/projekty/listStudents/views/AddView.java
@@ -19,8 +19,20 @@ public class AddView extends View{
     @Override
     protected void build() {
         System.out.println("Nie zaimplementaowana opcja dodawania");
-        Scanner skaner = new Scanner(System.in);
-        skaner.nextLine();
+        Scanner addScanner = new Scanner(System.in);
+
+        System.out.println("Podaj imię:");
+        String name = addScanner.nextLine();
+
+        System.out.println("Podaj nazwisko:");
+        String surname = addScanner.nextLine();
+
+        System.out.println("Podaj numer telefonu:");
+        String phoneNumber = addScanner.nextLine();
+
+        System.out.println("Dodano: "+name+" "+surname);
+        System.out.println("[Aby wrócić do menu wciśnij enter]");
+        addScanner.nextLine();
     }
 
     // konstruktor
